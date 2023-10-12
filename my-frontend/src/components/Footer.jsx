2d98cb2c5beca1ec62e03bfefcbccd2cf2bd4ee0
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-10-12 15:16:26 +0800

    Minor changes on Header and Footer section

--- my-frontend/src/components/Footer.jsx
+++ my-frontend/src/components/Footer.jsx
@@ -1,15 +1,21 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-20">
+    <footer className="bg-gray-800 py-20 inset-x-0 bottom-0">
       <div className="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
         <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12">
           <div className="text-xs uppercase text-gray-400 font-medium mb-6">
             Getting Started
           </div>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Installation
           </a>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Release Notes
           </a>
         </div>
@@ -18,10 +24,16 @@ function Footer() {
           <div className="text-xs uppercase text-gray-400 font-medium mb-6">
             Resources
           </div>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Documentation
           </a>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Tutorials
           </a>
         </div>
@@ -30,10 +42,16 @@ function Footer() {
           <div className="text-xs uppercase text-gray-400 font-medium mb-6">
             Company
           </div>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Home
           </a>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             About us
           </a>
         </div>
@@ -42,10 +60,16 @@ function Footer() {
           <div className="text-xs uppercase text-gray-400 font-medium mb-6">
             Legal
           </div>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Privacy Policy
           </a>
-          <a href="/" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+          <a
+            href="/"
+            className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+          >
             Terms of Service
           </a>
         </div>
--- my-frontend/src/components/Header.jsx
+++ my-frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -9,29 +9,54 @@ function Header() {
       <div className="flex items-center justify-between px-4 py-3 sm:p-0">
         <div>
           <Link to="/">
-            <img className="h-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+            <img
+              className="h-8"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              alt="Workflow"
+            />
           </Link>
         </div>
         <div className="sm:hidden">
-          <button type="button" className="block text-gray-500 hover:text-white focus:text-white focus:outline-none" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+          <button
+            type="button"
+            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          >
             <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path className="heroicon-ui"
-                d="M4.5 6.5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2zm0 5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2zm0 5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2z" />
+              <path
+                className="heroicon-ui"
+                d="M4.5 6.5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2zm0 5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2zm0 5h15a1 1 0 0 1 0 2h-15a1 1 0 0 1 0-2z"
+              />
             </svg>
           </button>
         </div>
       </div>
-      <nav className={`sm:flex sm:items-center sm:px-4 sm:py-2 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
-        <Link to="/"
-          className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700">Home</Link>
-        <Link to="/blog"
-          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700 sm:mt-0 sm:ml-2">Blog</Link>
-        <Link to="/dashboard"
-          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700 sm:mt-0 sm:ml-2">Dashboard</Link>
+      <nav
+        className={`sm:flex sm:items-center sm:px-4 sm:py-2 ${
+          isMobileMenuOpen ? "block" : "hidden"
+        }`}
+      >
+        <Link
+          to="/"
+          className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700"
+        >
+          Home
+        </Link>
+        <Link
+          to="/blog"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700 sm:mt-0 sm:ml-2"
+        >
+          Blog
+        </Link>
+        <Link
+          to="/dashboard"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-700 sm:mt-0 sm:ml-2"
+        >
+          Dashboard
+        </Link>
       </nav>
     </header>
   );
 }
 
 export default Header;
-

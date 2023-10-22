143d4d8f723d595dc73a5ef242ec5e530f2135b4
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-10-23 00:52:38 +0800

    Add to perform MongoDB Query

--- my-backend/src/models/BlogPost.js
+++ my-backend/src/models/BlogPost.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // Define the BlogPost schema
-const BlogSchema = new mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: String,
   content: String,
   author: String,
@@ -9,6 +9,15 @@ const BlogSchema = new mongoose.Schema({
 });
 
 // Create the BlogPost model
-const BlogPost = mongoose.model("blogpost", BlogSchema);
+const BlogPost = mongoose.model("blogpost", blogSchema);
+
+BlogPost.find({})
+  .select("_id")
+  .then((docs) => {
+    console.log(docs);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default BlogPost;

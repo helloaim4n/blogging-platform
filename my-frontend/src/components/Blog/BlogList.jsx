3b81cb0f8e3f04ce8658ca2a3dfbd440170103a7
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-10-27 23:12:24 +0800

    Update to fetch all the list in collection

--- my-frontend/src/components/Blog/BlogList.jsx
+++ my-frontend/src/components/Blog/BlogList.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function BlogList() {
   const [blogList, setBlogList] = useState([]);
+  // const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://127.0.0.1:5000/blogs");
-        console.log(response.data);
         setBlogList(response.data);
       } catch (err) {
         console.log(err);
@@ -19,45 +20,50 @@ function BlogList() {
     fetchData();
   }, []);
 
-  const viewPost = (id) => {
-    // Your logic for viewing a post goes here
-    console.log(`Viewing post with id: ${id}`);
-  };
+  // const viewPost = (_id) => {
+  //   navigate(`/blogs/${_id}`);
+  //   console.log(`Viewing post with id: ${_id}`);
+  // };
 
   return (
-    <div className="p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
       <h2 className="text-2xl font-bold mb-4">Blog Posts</h2>
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Published Date</th>
-            <th>Content</th>
-            <th>Author</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {blogList.map((post) => {
-            return (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>{post.publishedDate}</td>
-                <td>{post.author}</td>
-                <td>{post.content}</td>
-                <td>
-                  <button
-                    className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
-                    onClick={() => viewPost(post.id)}
-                  >
-                    View Post
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="overflow-x-auto">
+        <table className="table-auto w-full text-center mx-auto">
+          <thead className="bg-blue-500 text-white ">
+            <tr>
+              <th className="px-4 py-2">Title</th>
+              <th className="px-4 py-2">Author</th>
+              <th className="px-4 py-2">Content</th>
+              <th className="px-4 py-2">Published Date</th>
+              <th className="px-4 py-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogList.map((post) => {
+              return (
+                <tr
+                  key={post._id}
+                  className="border-b border-gray-200 bg-gray-50"
+                >
+                  <td className="px-4 py-2">{post.title}</td>
+                  <td className="px-4 py-2">{post.author}</td>
+                  <td className="px-4 py-2">{post.content}</td>
+                  <td className="px-4 py-2">{post.publishedDate}</td>
+                  <td className="px-4 py-2">
+                    <Link
+                      to={`/blogs/${post._id}`}
+                      className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      View Post
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
